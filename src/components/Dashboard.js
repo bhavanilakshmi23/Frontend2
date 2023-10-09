fe6ffeedbc76e1b2d4cec604e2891f38745c9c76
bhavani <127599832+bhavanilakshmi23@users.noreply.github.com>
2023-10-09 21:51:37 +0530

    Add files via upload

---
+++ src/components/Dashboard.js
@@ -0,0 +1,200 @@
+
+import React, {useState} from 'react'
+import Table from './Table'
+
+const Dashboard = () => {
+const[showInvoice,setShowInvoice]=useState(false)
+const[name,setName]=useState('')
+const[address,setAddress]=useState('')
+const[invoiceNo,setInvoiceNo]=useState('')
+const[invoiceDate,setInvoiceDate]=useState('')
+const[storeName,setStoreName]=useState('')
+const[storeAddress,setStoreAddress]=useState('')
+const[email,setEmail]=useState('')
+const[phoneNo,setPhoneNo]=useState('')
+const[notes,setNotes]=useState('')
+const[description,setDescription]=useState('')
+const[quantity,setQuantity]=useState('')
+const[price,setPrice]=useState('')
+const[amount,setAmount]=useState('')
+const[list,setList]=useState([])
+const[total,setTotal]=useState(0)
+
+
+const handleprint=()=>{
+  window.print()
+}
+
+  return (
+    <> 
+      <div>
+  {showInvoice ? (<div className='container'>
+    <header>
+      <div>
+     <h1>Invoice</h1></div>
+      <div className='part1'>
+      <div className='part3'>
+      <h5>Billed To</h5>
+      <span>{name}</span><br/>
+      <span>{address}</span>
+      </div>
+     <div className='part2'>
+      <h6>Invoice __No:<span>{invoiceNo}</span></h6>
+     <h6>Invoice Date:<span>{invoiceDate}</span></h6>
+      </div>
+      </div>
+    </header>
+    <main>
+      <div>
+      <table className="table">
+    <thead className="table-light">
+     <tr>
+        <td>Description</td>
+        <td>Quantity</td>
+        <td>Price</td>
+        <td>Amount</td>
+     </tr>
+    </thead>
+    {list.map(({id,description,quantity,price,amount})=>{return(
+           
+  <tbody>
+      <tr key={id}>
+        <td>{description}</td>
+        <td>{quantity}</td>
+        <td>{price}</td>
+        <td>{amount}</td>
+      </tr>
+    </tbody>
+      
+    )})}
+    </table>
+    <div className='total'>
+        <h2>Rs.{total.toLocaleString()}</h2>
+      </div>
+      </div>
+      
+   <div className='notes'>
+    <p>{notes}</p>
+   </div>
+    </main>
+    <footer>
+      <div className='footer'>
+        <h2 >{storeName}</h2>
+        <span>{storeAddress}</span><br/>
+        <span>{email}</span><br/>
+        <span>{phoneNo}</span>
+      </div>
+      <input type='submit' value='print' onClick={handleprint}/>
+     <input type="submit" value='Create Invoice'  onClick={()=>setShowInvoice(false)}/>
+    </footer>
+  
+   </div>  
+    ):(
+    <>
+   <div className='form'>
+  <label>Customer Name:  
+        <input
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        /> </label>
+     
+   <label>Customer Address:
+   
+        <input
+          type="text" 
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+          required
+        /> </label>
+      <label>Invoice No: 
+        <input
+          type="text" 
+          value={invoiceNo}
+          onChange={(e) => setInvoiceNo(e.target.value)}
+          required
+        /></label>
+     
+      <label>Invoice Date: 
+        <input
+          type="date" 
+          value={invoiceDate}
+          onChange={(e) => setInvoiceDate(e.target.value)}
+          required
+        />
+   </label>
+   <Table
+   description={description}
+   setDescription={setDescription}
+   quantity={quantity}
+   setQuantity={setQuantity}
+   price={price}
+   setPrice={setPrice}
+   amount={amount}
+   setAmount={setAmount}
+   list={list}
+   setList={setList}
+   total={total}
+   setTotal={setTotal}
+   />
+
+      <label>Store Name:
+        <input
+          type="text" 
+          value={storeName}
+          onChange={(e) => setStoreName(e.target.value)}
+          required
+        /></label>
+      
+      <label>Store Email: 
+        <input
+          type="text" 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        /> </label>
+    
+      <label>Store PhoneNo:
+        <input
+          type="text" 
+          value={phoneNo}
+          onChange={(e) => setPhoneNo(e.target.value)}
+          required
+        /></label>
+      
+      <label>Store Addreess:
+        <input
+          type="text" 
+          value={storeAddress}
+          onChange={(e) => setStoreAddress(e.target.value)}
+          required
+        />
+       </label>
+      
+      <label>Additional Notes: 
+        <input
+          type="text" 
+          value={notes}
+          onChange={(e) => setNotes(e.target.value)}
+          required
+        />
+     </label>
+
+      {/* <button onClick={()=>setShowInvoice(true)}>showInvoice</button>  */}
+      <input type="submit" value="ShowInvoice"
+      onClick={()=>setShowInvoice(true)} className='showinvoice'
+      />
+      
+    
+   </div>
+  
+  </>)
+}
+</div>
+ </>
+      
+  )
+}
+
+export default Dashboard
---
+++ src/components/Table.js
@@ -0,0 +1,121 @@
+import React, { useEffect,useState } from 'react'
+import {AiOutlineDelete} from 'react-icons/ai'
+import {v4 as uuidv4} from "uuid"
+
+const Table = ({description,setAmount,amount,setDescription,price,setPrice,quantity,setQuantity,list,setList,total,setTotal}) => {
+
+
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        const newItem={
+            id: uuidv4(),
+           description,
+            quantity,
+         price,
+           amount  
+        }
+        setDescription("")
+        setQuantity("")
+        setPrice("")
+        setAmount("")
+       setList([...list,newItem])
+     
+      
+    }
+
+
+
+  useEffect(()=>{
+    const calculateAmount=(amount)=>{
+       setAmount(quantity*price)
+      }
+      calculateAmount(amount)
+  },[quantity,price,amount,setAmount])
+
+  let rows=document.querySelectorAll('.amount')
+  let sum=0
+
+  for(let i=0;i<rows.length;i++){
+    if(rows[i].className==="amount"){
+        sum+= isNaN(rows[i].innerHTML)? 0 : parseInt(rows[i].innerHTML)
+        setTotal(sum)
+    }
+  }
+
+ 
+
+const  deleteRow=(id)=> setList(list.filter((row)=>row.id!==id))
+
+ 
+    return (
+   <>
+   <form onSubmit={handleSubmit}>
+    <label>Description:  
+        <input
+          type="text" 
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        /> </label>
+     
+   <label>Quantity:
+   
+        <input
+          type="text" 
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        /> </label>
+      <label>Price: 
+        <input
+          type="text" 
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        /></label>
+     
+      <label>Amount: 
+       <p>{amount}</p>
+   </label>
+<button  type='submit' >
+    
+  Add Table Item </button>
+
+   </form>
+   
+   <table className="table">
+    <thead className="table-light">
+     <tr>
+        <td>Description</td>
+        <td>Quantity</td>
+        <td>Price</td>
+        <td >Amount</td>
+     </tr>
+    </thead>
+    {list.map(({id,description,quantity,price,amount})=>{return(
+           
+  <tbody>
+      <tr key={id}>
+        <td>{description}</td>
+        <td>{quantity}</td>
+        <td>{price}</td>
+        <td className='amount'>{amount}</td>
+        <td><button onClick={()=>deleteRow(id)}><AiOutlineDelete
+        className='delete'/></button></td>
+       
+      </tr>
+    </tbody>
+      
+    )})}
+    </table>
+ 
+      <div className='total'>
+        <h2>Rs.{total.toLocaleString()}</h2>
+      </div>
+   </>
+  )
+}
+
+export default Table
+
+
+
+
+
